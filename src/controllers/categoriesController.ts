2220c21a0127c6597d12c413a92722d1c97a50ff
Angelie <angelie.angman@gmail.com>
2026-08-04 14:38:47 +0200

    Implement GET category by id

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -15,3 +15,21 @@ export const getAllCategories = async (req: Request, res: Response) => {
     res.status(500).json({ error: message })
   }
 }
+
+export const getCategory = async (req: Request, res: Response) => {
+  const id = req.params.id
+
+  try {
+    const sql = `
+
+        SELECT * FROM categories 
+        WHERE id = ?
+        `
+
+    const [result] = await db.query(sql, [id])
+    res.json(result)
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : 'Unknown error'
+    res.status(500).json({ error: message })
+  }
+}
--- src/routes/categoriesRouter.ts
+++ src/routes/categoriesRouter.ts
@@ -1,8 +1,12 @@
 import express from 'express'
-import { getAllCategories } from '../controllers/categoriesController.js'
+import {
+  getAllCategories,
+  getCategory,
+} from '../controllers/categoriesController.js'
 const router = express.Router()
 
 // Endpoints
 router.get('/', getAllCategories)
+router.get('/:id', getCategory)
 
 export default router
