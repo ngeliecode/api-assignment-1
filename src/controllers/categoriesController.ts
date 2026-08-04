2ebf95f12f582b1e7d0d6648e4ee3c37e2987dc0
Angelie <angelie.angman@gmail.com>
2026-08-04 19:18:55 +0200

    Implement POST and  PATCH /categories endpoint

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -5,8 +5,8 @@ export const getAllCategories = async (req: Request, res: Response) => {
   try {
     const sql = `
 
-    SELECT * FROM categories
-    `
+            SELECT * FROM categories
+        `
 
     const [result] = await db.query(sql)
     res.json(result)
@@ -22,11 +22,49 @@ export const getCategory = async (req: Request, res: Response) => {
   try {
     const sql = `
 
-        SELECT * FROM categories 
-        WHERE id = ?
+            SELECT * FROM categories 
+            WHERE id = ?
         `
 
     const [result] = await db.query(sql, [id])
+    res.json(result)
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : 'Unknown error'
+    res.status(500).json({ error: message })
+  }
+}
+
+export const createCategory = async (req: Request, res: Response) => {
+  const name = req.body.name
+
+  try {
+    const sql = `
+
+            INSERT INTO categories (name)
+            VALUES (?)
+        `
+
+    const [result] = await db.query(sql, [name])
+    res.json(result)
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : 'Unknown error'
+    res.status(500).json({ error: message })
+  }
+}
+
+export const updateCategory = async (req: Request, res: Response) => {
+  const id = req.params.id
+  const name = req.body.name
+
+  try {
+    const sql = `
+
+            UPDATE categories
+            SET name = ?
+            WHERE id = ?
+        `
+
+    const [result] = await db.query(sql, [name, id])
     res.json(result)
   } catch (error: unknown) {
     const message = error instanceof Error ? error.message : 'Unknown error'
--- src/routes/categoriesRouter.ts
+++ src/routes/categoriesRouter.ts
@@ -1,12 +1,16 @@
 import express from 'express'
 import {
+  createCategory,
   getAllCategories,
   getCategory,
+  updateCategory,
 } from '../controllers/categoriesController.js'
 const router = express.Router()
 
 // Endpoints
 router.get('/', getAllCategories)
 router.get('/:id', getCategory)
+router.post('/', createCategory)
+router.patch('/:id', updateCategory)
 
 export default router
