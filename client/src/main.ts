9f48b2d8d1314e2012a36e545b81d4ec1b453d53
Angelie <angelie.angman@gmail.com>
2026-08-18 17:11:15 +0200

    Create HTML for products

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,27 @@
 import './style.css'
+import { bookInfo } from './products'
+
+const productsContainer = document.querySelector('.products')
 
 const getProducts = async () => {
   const response = await fetch('http://localhost:3000/products')
   const products = await response.json()
 
   console.log(products)
+
+  products.forEach((product) => {
+    const info = bookInfo[product.id]
+    productsContainer.innerHTML += `
+    
+      <div class="product">
+        <img src="${product.image}" alt="${product.title}">
+        <h2>${product.title}</h2>
+        <p>${info.author}</p>
+        <p>${info.format}</p>
+        <p>${Number(product.price)} kr</p>
+      </div>
+    `
+  })
 }
 
 getProducts()
---
+++ client/src/products.ts
@@ -0,0 +1,14 @@
+export const bookInfo = {
+  1: {
+    author: 'Valérie Perrin',
+    format: 'Inbunden',
+  },
+  2: {
+    author: 'Gillian Flynn',
+    format: 'Storpocket',
+  },
+  3: {
+    author: 'Riley Sager',
+    format: 'Kartonnage',
+  },
+}
