ef170313fac88aeac34baed0420f9d13d2d574e4
Angelie <angelie.angman@gmail.com>
2026-08-19 11:04:23 +0200

    Implement product search

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,26 +1,40 @@
 import './style.css'
 import { bookInfo } from './products'
 
-const productsContainer = document.querySelector('.products')
+const getProducts = async (search = '') => {
+  const productsContainer = document.querySelector('.products')
+  const searchForm = document.querySelector('.search-form form')
 
-const getProducts = async () => {
-  const response = await fetch('http://localhost:3000/products')
+  const response = await fetch(
+    `http://localhost:3000/products?search=${search}`,
+  )
+
   const products = await response.json()
 
-  console.log(products)
+  productsContainer.innerHTML = ''
 
   products.forEach((product) => {
     const info = bookInfo[product.id]
+
     productsContainer.innerHTML += `
-    
       <div class="product">
         <img src="${product.image}" alt="${product.title}">
         <h2>${product.title}</h2>
         <p>${info.author}</p>
         <p>${info.format}</p>
         <p>${Number(product.price)} kr</p>
+        <button class="addBtn">Lägg till</button>
       </div>
     `
+  })
+
+  searchForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const searchInput = document.querySelector('#search') as HTMLInputElement
+    const search = searchInput.value
+
+    getProducts(search)
   })
 }
 
