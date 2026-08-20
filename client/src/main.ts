990f6e46dd471384230d3b5f0fe72880a41bd849
Angelie <angelie.angman@gmail.com>
2026-08-20 20:40:46 +0200

    Implement mobile navigation menu

--- client/src/main.ts
+++ client/src/main.ts
@@ -27,3 +27,12 @@ const closeSearch = document.querySelector('.close-search')
 closeSearch?.addEventListener('click', () => {
   header?.classList.remove('search-open')
 })
+
+const menuButton = document.querySelector('.menu-button')!
+const menu = document.querySelector('.menu')!
+
+menuButton.addEventListener('click', () => {
+  menuButton.classList.toggle('open')
+  menu.classList.toggle('open')
+  document.body.classList.toggle('menu-open')
+})
