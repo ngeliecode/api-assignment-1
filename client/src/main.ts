d456bf546c2c3883b9e9aabe6aade3ecac412d7c
Angelie <angelie.angman@gmail.com>
2026-08-21 11:30:43 +0200

    Implement cart overlay toggle

--- client/src/main.ts
+++ client/src/main.ts
@@ -28,6 +28,7 @@ closeSearch?.addEventListener('click', () => {
   header?.classList.remove('search-open')
 })
 
+// Knappen förvandlas till en annan knapp
 const menuButton = document.querySelector('.menu-button')!
 const menu = document.querySelector('.menu')!
 
@@ -35,4 +36,14 @@ menuButton.addEventListener('click', () => {
   menuButton.classList.toggle('open')
   menu.classList.toggle('open')
   document.body.classList.toggle('menu-open')
+})
+
+// Båda knapparna är kvar
+const checkoutButton = document.querySelector('.checkout-button')
+const checkout = document.querySelector('.checkout')
+
+checkoutButton.addEventListener('click', () => {
+  checkoutButton.classList.toggle('open')
+  checkout.classList.toggle('open')
+  document.body.classList.toggle('checkout-open')
 })
