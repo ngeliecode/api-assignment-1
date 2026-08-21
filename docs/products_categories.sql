b3b3012b35b4cfa0c78d0ced988459bfcda54c4d
Angelie <angelie.angman@gmail.com>
2026-08-21 14:10:39 +0200

    Update SQL export files

--- docs/products_categories.sql
+++ docs/products_categories.sql
@@ -1,6 +1,9 @@
 insert into `products_categories` (`category_id`, `product_id`) values (1, 2);
 insert into `products_categories` (`category_id`, `product_id`) values (1, 3);
+insert into `products_categories` (`category_id`, `product_id`) values (2, 27);
 insert into `products_categories` (`category_id`, `product_id`) values (6, 3);
-insert into `products_categories` (`category_id`, `product_id`) values (8, 1);
+insert into `products_categories` (`category_id`, `product_id`) values (7, 27);
 insert into `products_categories` (`category_id`, `product_id`) values (9, 2);
 insert into `products_categories` (`category_id`, `product_id`) values (9, 3);
+insert into `products_categories` (`category_id`, `product_id`) values (9, 26);
+insert into `products_categories` (`category_id`, `product_id`) values (9, 28);
