b3db3ff8dbc43b735d031080d41e077bccca8d87
Angelie <angelie.angman@gmail.com>
2026-09-03 21:05:17 +0200

    refactor renderProducts

---
+++ client/src/ui/renderProducts.ts
@@ -0,0 +1,16 @@
+export const renderProducts = (products) => {
+  const productsContainer = document.querySelector<HTMLElement>('.products')
+
+  productsContainer.innerHTML = ''
+
+  products.forEach((product) => {
+    productsContainer.innerHTML += `
+      <div class="product">
+        <img src="${product.image}" alt="${product.title}">
+        <h2>${product.title}</h2>
+        <p>${product.author}</p>
+        <p>${Number(product.price)} kr</p>
+      </div>
+    `
+  })
+}
