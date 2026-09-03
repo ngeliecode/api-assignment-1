5b439023a4a122241fa6832a34eb2bcc94134006
Angelie <angelie.angman@gmail.com>
2026-09-03 21:06:27 +0200

    refactor: move search form into search.ts

--- client/src/ui/search.ts
+++ client/src/ui/search.ts
@@ -1,3 +1,5 @@
+import { getProducts } from '../api'
+
 export const initSearch = () => {
   const header = document.querySelector('header')
   const openSearch = document.querySelector('.search-icon')
@@ -11,3 +13,14 @@ export const initSearch = () => {
     header?.classList.remove('search-open')
   })
 }
+
+const searchForm = document.querySelector('.search-form')
+
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const searchInput = document.querySelector('#search') as HTMLInputElement
+  const search = searchInput.value
+
+  getProducts(search, '')
+})
