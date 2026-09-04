cb143acaf0620213b79789238665ca525ff42dd5
Angelie <angelie.angman@gmail.com>
2026-09-04 12:08:00 +0200

    refactor: use map instead of forEach in renderProducts

--- client/src/ui/renderProducts.ts
+++ client/src/ui/renderProducts.ts
@@ -1,16 +1,14 @@
 export function renderProducts(products) {
   const productsContainer = document.querySelector<HTMLElement>('.products')
-
-  productsContainer.innerHTML = ''
-
-  products.forEach((product) => {
-    productsContainer.innerHTML += `
+  productsContainer.innerHTML = products
+    .map(
+      (product) => `     
       <div class="product">
         <img src="${product.image}" alt="${product.title}">
         <h2>${product.title}</h2>
         <p>${product.author}</p>
         <p>${Number(product.price)} kr</p>
-      </div>
-    `
-  })
+      </div>`,
+    )
+    .join('')
 }
